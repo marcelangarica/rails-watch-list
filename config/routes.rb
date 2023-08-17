b6921dc12d205601c001c514c163a36952d29f84
Marcela Langarica Mejia <mlangaricamejia@gmail.com>
2023-08-17 15:37:01 -0400

    List routing completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # root to: 'list#index'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "lists#index"
+
+  # get 'lists', to: 'lists#index'
+  # # get 'lists/new', to: 'lists#new', as: :new_list
+  # # post 'lists', to: 'lists#create'
+  # get 'lists/:id', to: 'lists#show', as: :list
+  resources :lists
+  resources :bookmarks
 end
